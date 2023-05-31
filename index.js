fd08f0499d248146e13a678fed661c1da536c8a1
kabi-r <programmerkabirr@gmail.com>
2023-05-31 12:44:26 +0600

    server user dashbord added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0i3pjbq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -31,6 +30,7 @@ async function run() {
 
     const menuCollection = client.db('BissroBossDb').collection('menu')
     const reviewCollection = client.db('BissroBossDb').collection('reviews')
+    const cartCollection = client.db('BissroBossDb').collection('carts')
 
     app.get('/menu', async(req, res) =>{
       const result = await menuCollection.find().toArray()
@@ -38,6 +38,35 @@ async function run() {
     })
     app.get('/review', async(req, res) =>{
       const result = await  reviewCollection.find().toArray()
+      res.send(result)
+    })
+
+    // Carts collection
+
+
+    app.post ('/carts', async(req, res) =>{
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item)
+      res.send(result)
+    })
+
+    app.get('/carts', async(req, res) =>{
+      const email = req.query.email;
+      console.log(email);
+      if(!email){
+        return []
+      }
+      const step = {email : email}
+     const result = await cartCollection.find(step).toArray()
+    // const result = await cartCollection.find().toArray()
+     res.send(result)
+    })
+
+    app.delete('/carts/:id', async(req,res) =>{
+      const id = req.params.id
+      const query = {_id:new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
